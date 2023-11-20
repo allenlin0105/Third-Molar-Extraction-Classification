6672af7ce1666a357442bec5ef6e564bb5c8d344
allenlin0105 <b08902053@csie.ntu.edu.tw>
2023-11-20 06:05:19 +0000

    add assertion on prediction shape, add comment on sample model

--- classification/common/base_model.py
+++ classification/common/base_model.py
@@ -9,14 +9,14 @@ class BaseModel(pl.LightningModule):
         super().__init__()
         self.lr = args.lr
         self.weight_decay = args.weight_decay
+        self.loss_func = nn.CrossEntropyLoss()
+
+        self.save_hyperparameters()
 
         """
         You can change self.model here with your model
         """
         self.model = CNN(args.image_size)
-        self.loss_func = nn.CrossEntropyLoss()
-
-        self.save_hyperparameters()
     
     """
     Custom functions
@@ -36,7 +36,14 @@ class BaseModel(pl.LightningModule):
     """
     def forward(self, batch):
         images = batch[0]
+        batch_size = images.size()[0]
+        
         pred = self.model(images)
+
+        # Check the prediction is in correct shape
+        assert pred.size()[0] == batch_size, "Your prediction does not have the correct batch_size at dimension 0"
+        assert pred.size()[1] == 3, "Your prediction should have 3 probabilities at dimension 1"
+
         return pred
 
     def training_step(self, batch, batch_idx):
@@ -50,7 +57,7 @@ class BaseModel(pl.LightningModule):
         return loss
 
     def predict_step(self, batch):
-        return self.forward_step(batch)
+        return self(batch)
 
     def configure_optimizers(self):
         optimizer = torch.optim.AdamW(
--- classification/models/CNN.py
+++ classification/models/CNN.py
@@ -1,8 +1,9 @@
 import math
+import torch
 from torch import nn
 
 class CNN(nn.Module):
-    def __init__(self, image_size): 
+    def __init__(self, image_size: int): 
         super().__init__()
         self.cnn1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=0)
         self.maxpool1 = nn.MaxPool2d(kernel_size=2)
@@ -19,7 +20,11 @@ class CNN(nn.Module):
         self.flatten = nn.Flatten()
         self.fc = nn.Linear(32 * image_size * image_size, 3) 
     
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
+        """
+        x: a batch of images with shape (batch_size, 3, image_size, image_size)
+        return: a batch of predicted probabilities with shape (batch_size, 3)
+        """
         x = x.float()
         x = self.cnn1(x)
         x = self.act(x)
